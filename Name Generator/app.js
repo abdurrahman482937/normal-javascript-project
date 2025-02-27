edbdeb0e46d292634e2e86cc811e520f46c95370
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-02-27 21:44:20 +0600

    Basic Name Generator with out Array

---
+++ Name Generator/app.js
@@ -0,0 +1,31 @@
+let random = Math.random();
+let a, b, c;
+
+random = Math.random();
+if (random < 0.33) {
+  a = "crazy";
+} else if (random < 0.66 && random >= 0.33) {
+  a = "amazing";
+} else {
+  a = "fire";
+}
+
+random = Math.random();
+if (random < 0.33) {
+  b = "engine";
+} else if (random < 0.66 && random >= 0.33) {
+  b = "food";
+} else {
+  b = "garments";
+}
+
+random = Math.random();
+if (random < 0.33) {
+  c = "bros";
+} else if (random < 0.66 && random >= 0.33) {
+  c = "limited";
+} else {
+  c = "hub";
+}
+
+console.log(a + " " + b + " " + c)
